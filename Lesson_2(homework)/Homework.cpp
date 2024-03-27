37df611865ee74665f03d0ad02cc1c5a44f67672
villsun <lonukkola@gmail.com>
2024-03-27 22:07:48 +0100

    Add class String

--- Lesson_2(homework)/Homework.cpp
+++ Lesson_2(homework)/Homework.cpp
@@ -4,6 +4,50 @@
 
 using namespace std;
 
+class String
+{
+private:
+	char* p_str;
+public:
+	String()
+	{
+		p_str = (char*)malloc(80 * sizeof(char));
+		strcpy(p_str, "None");
+	}
+	String(int size)
+	{
+		p_str = (char*)malloc(size * sizeof(char));
+		strcpy(p_str, "None");
+	}
+	String(char* str)
+	{
+		p_str = (char*)malloc(strlen(str) * sizeof(char) + 1);
+		strcpy(p_str, str);
+	}
+	String(String& old_str)
+	{
+		p_str = (char*)malloc(strlen(old_str.p_str) * sizeof(char) + 1);
+		strcpy(p_str, old_str.p_str);
+	}
+
+	void Show_Str()
+	{
+		cout << p_str << endl;
+	}
+
+	void Enter_Str()
+	{
+		char p_str[200];
+		cin >> p_str;
+		this->p_str = (char*)realloc(this->p_str, strlen(p_str) * sizeof(char) + 1);
+		strcpy(this->p_str, p_str);
+	}
+	~String()
+	{
+		free(p_str);
+	}
+};
+
 int main()
 {
 
